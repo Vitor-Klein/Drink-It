b28126af25236cc7ac4126342fc1a0a2de509d61
Gabriel Trojack <trojackgabriel@gmail.com>
2022-05-14 15:31:25 -0300

    Mais cards

--- src/cards.js
+++ src/cards.js
@@ -71,6 +71,66 @@ export const cards = {
       title: "Confiança",
       imagePath: require('./assets/drunkHappy.png'),
       description: "deixe a pessoa a sua direita mandar uma mensagen para qualquer um de seus contatos ou beba 3 doses"
+    },
+    {
+      id: "13",
+      title: "Gas helio",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Escolha alguem, essa pessoa tem que falar com uma voz fina por 3 rodadas ou beber 2 shots"
+    },
+    {
+      id: "14",
+      title: "Sexy dance!!!",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Faça uma dança no colo de alguem ou beba 2 shots"
+    },
+    {
+      id: "15",
+      title: "Parabens!É um menino!",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Simule um parto de uma garrafa ou beba 3 shots"
+    },
+    {
+      id: "16",
+      title: "Muuuuuuuuu",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Finja que é uma vaca e deixe alguem te ordenhar ou beba 2 shots"
+    },
+    {
+      id: "17",
+      title: "Confiança",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "deixe a pessoa a sua direita mandar uma mensagen para qualquer um de seus contatos ou beba 3 doses"
+    },
+    {
+      id: "18",
+      title: "Stand up",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Tente fazer a pessoa mais proxima de você rir, se falhar tome 1 dose"
+    },
+    {
+      id: "19",
+      title: "Slow motion",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Dance em camera lenta "
+    },
+    {
+      id: "20",
+      title: "Imitose",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Imite a pessoa a sua esquerda por 2 rodadas ou beba 2 doses"
+    },
+    {
+      id: "21",
+      title: "Maromba",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Escolha uma pessoa, a cada 5 flexões que você fizer ela bebe 1 shot, se não chegar a 5 flexões você bebe 2 shots"
+    },
+    {
+      id: "22",
+      title: "Babão",
+      imagePath: require('./assets/drunkHappy.png'),
+      description: "Pague um boquete para uma garrafa"
     }
   ]
 }
